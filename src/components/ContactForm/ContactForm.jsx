9284c46ab0c7f0b2b5d355d7deab7cce0efa2a56
Yura Shaplavsky <yurikorock777@gmail.com>
2025-04-25 11:13:34 +0300

    add validation to form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,57 @@
 //ContactForm.jsx
 import { Formik, Form, Field } from "formik";
 import { nanoid } from "nanoid";
+import { useId } from "react";
+import * as Yup from "yup";
+import { ErrorMessage } from "formik";
 
 const ContactForm = ({ onAdd }) => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
   const handleSubmit = (values, actions) => {
     onAdd({
       name: values.name,
       number: values.number,
       id: nanoid(),
     });
+
     actions.resetForm();
   };
+  //валідація даних в формі
+  const FeedbackSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
+
   return (
-    <Formik initialValues={{ name: "", number: "" }} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form>
-        <Field type="text" name="name" placeholder="Name" />
-        <Field type="text" name="number" placeholder="Number" />
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" placeholder="Name" id={nameFieldId} />
+          <ErrorMessage name="name" component="span" />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field
+            type="text"
+            name="number"
+            placeholder="Number"
+            id={numberFieldId}
+          />
+          <ErrorMessage name="number" component="span" />
+        </div>
         <button type="submit">Add contact</button>
       </Form>
     </Formik>
